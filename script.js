f1ed5d58d3d52a788f6930f6a734e8983af5edb8
Jared <jemraider21@gmail.com>
2020-07-28 15:32:00 -0400

    Added quote and author to UI, added twitter and new quote functionallity, and created a preview thumbnail

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 // Get quote from API
 async function getQuote(){
-    const proxyURL = "https://cors-anywhere.herokuapp.com/";
-    const apiURL = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+    const proxyURL = "https://damp-brushlands-66931.herokuapp.com/"; // Personal proxy server
+    const apiURL = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
     try{
         const response = await fetch(proxyURL + apiURL);
-        const data = await response.json;
-        console.log(data);
+        const data = await response.json();
+
+        // Set authorText to Unknown if there is no author, else set to author name
+        if(data.quoteAuthor === ""){
+            authorText.innerText = "Unknown";
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        
+        // Reduce the font size for long quote
+        if(data.quoteText.length > 120){
+            quoteText.classList.add("long-quote");
+        } else {
+            quoteText.classList.remove("long-quote");
+        }
+        quoteText.innerText = data.quoteText;
     } catch(error){
         getQuote();
-        console.log("Whoops, no quote", error);
     }
 }
+
+// Tweet quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterURL = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterURL, "_blank");
+}
+
+// Event listeners
+newQuoteBtn.addEventListener("click", getQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 // On load
 getQuote();
