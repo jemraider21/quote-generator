636276feaa4218870144ae3b8ddec5b0c229e8d9
Jared <jemraider21@gmail.com>
2020-07-28 14:41:51 -0400

    Connected the javascript to the fetch quote api

---
+++ script.js
@@ -0,0 +1,16 @@
+// Get quote from API
+async function getQuote(){
+    const proxyURL = "https://cors-anywhere.herokuapp.com/";
+    const apiURL = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+    try{
+        const response = await fetch(proxyURL + apiURL);
+        const data = await response.json;
+        console.log(data);
+    } catch(error){
+        getQuote();
+        console.log("Whoops, no quote", error);
+    }
+}
+
+// On load
+getQuote();
